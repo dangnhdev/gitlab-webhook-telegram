9abbffce1f4656b7f6fde0a65d743fcf79b9f749
Nguyen Hai Dang <dang.gffg@gmail.com>
2023-05-14 15:29:44 +0700

    feat: handle branch created & removed event

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -9,41 +9,62 @@ const GITLAB_WEBHOOK_TOKEN = process.env.GITLAB_WEBHOOK_TOKEN
 const app = express()
 app.use(express.json())
 
-app.post('/gitlabHook', async function (request, response) {
+app.use((request, response, next) => {
     const gitlabToken = request.headers['x-gitlab-token']
     if (gitlabToken && (gitlabToken != GITLAB_WEBHOOK_TOKEN)) {
         response.status(401).send('Wrong auth token')
         return
     }
-    if (request.headers['x-gitlab-event'] == 'Push Hook') {
-        const pushInfo = request.body
-        // prettier-ignore
-        const message = `<b>[<a href="${pushInfo.project.homepage}">${pushInfo.project.name}</a>]</b> <b>${pushInfo.user_name}</b> pushed ${pushInfo.total_commits_count} commits\n`
-        // prettier-ignore
-        const commitMessage = pushInfo.commits.map(commit =>
-            `• <a href="${commit.url}">${commit.message.trim()}</a>`.replace(/\n\n/gi, '\n')
-        )
-            .join('\n').trim()
-        const botResponse = await sendTelegramMessage(message + commitMessage)
+    next()
+})
 
-        if (!botResponse.ok) {
-            const responseBody = botResponse.body.read().toString()
-            functions.logger.error(
-                'Failed to send telegram message',
-                responseBody
-            )
-            response.status(botResponse.status).end(JSON.stringify({
-                telegram_response: responseBody,
-                chat_message: message + commitMessage
-            }))
-            return
-        }
-        response.sendStatus(200)
+app.post('/gitlabHook', async function (request, response) {
+    if (request.headers['x-gitlab-event'] != 'Push Hook') {
+        response.status(501).send('Not supported gitlab event')
         return
     }
 
-    response.status(501).send('Not supported gitlab event')
+    const message = generateMessage(request.body)
+    const botResponse = await sendTelegramMessage(message)
+
+    if (!botResponse.ok) {
+        const responseBody = botResponse.body.read().toString()
+        functions.logger.error(
+            'Failed to send telegram message',
+            responseBody
+        )
+        response.status(botResponse.status).end(JSON.stringify({
+            telegram_response: responseBody,
+            chat_message: message
+        }))
+        return
+    }
+    response.sendStatus(200)
+    return
+
 })
+
+function generateMessage(pushInfo: any){
+    const branchName = pushInfo.ref.split('/').pop()
+    const introMessage = `<b>[<a href="${pushInfo.project.homepage}">${pushInfo.project.name}</a>]</b> <b>${pushInfo.user_name}</b>`
+
+    //handle branch deleted
+    if (pushInfo.after == '0000000000000000000000000000000000000000' && !pushInfo.checkout_sha){
+        return `${introMessage} deleted branch ${branchName}`
+    }
+
+    if (pushInfo.before == '0000000000000000000000000000000000000000'){
+        return `${introMessage} created branch ${branchName}`
+    }
+
+    //handle commit pushed event
+    const message = `${introMessage} pushed ${pushInfo.total_commits_count} ${pushInfo.total_commits_count > 1? 'commits' : 'commit'} to branch ${branchName}\n`
+    const commitMessage = pushInfo.commits.map(commit =>
+        `• <a href="${commit.url}">${commit.message.trim()}</a>`.replace(/\n\n/gi, '\n')
+    )
+        .join('\n').trim()
+    return message + commitMessage
+}
 
 function sendTelegramMessage(message: string): Promise<Response> {
     return fetch(
